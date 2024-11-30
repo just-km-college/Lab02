afa47342ed3e97a0b8d750ba1a470fe08dfb74e9
karol <karol.majda@student.pk.edu.pl>
2024-11-30 10:28:03 +0100

    Commit

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,17 @@ public class Main {
         Color color3 = new Color(90,-100,300,4000);
         Color color4 = new Color(-200,5000,300);
 
+        // niceToString showcase
+        System.out.printf("Color 1: %s \n",color1.niceToString());
+        System.out.printf("Color 2: %s \n",color2.niceToString());
+        System.out.printf("Color 3: %s \n",color3.niceToString());
+        System.out.printf("Color 4: %s \n\n",color4.niceToString());
+
         // toString showcase
         System.out.printf("Color 1: %s \n",color1);
         System.out.printf("Color 2: %s \n",color2);
         System.out.printf("Color 3: %s \n",color3);
-        System.out.printf("Color 4: %s \n",color4);
+        System.out.printf("Color 4: %s \n\n",color4);
 
         // getters showcase
         System.out.printf("Color 1: a: %d, R: %d, G: %d, B: %d\n",color1.alfa(), color1.red(), color1.green(), color1.blue());
